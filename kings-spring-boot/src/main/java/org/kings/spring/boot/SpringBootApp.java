a28de7ec5c639fcb2dd821fc2dbcbc5c9ab49a3e
za-wanglun <lun.wang@zatech.com>
2020-07-24 18:43:55 +0800

    [KINGS-2] commit the framework by design 2

---
+++ kings-spring-boot/src/main/java/org/kings/spring/boot/SpringBootApp.java
@@ -0,0 +1,15 @@
+package org.kings.spring.boot;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author lun.kings
+ * @date 2020/7/19 1:33 下午
+ * @email lun.kings@zatech.com
+ * @since
+ */
+@SpringBoot
+public class SpringBootApp {
+}
